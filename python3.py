a61074bb36a11e9e6ac5ab28a39c3bbe537e5fc5
Asma-javeed <asmiijaveed705@gmail.com>
2025-04-23 21:09:06 +0500

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,43 @@
+# program no 1:
+# nested if else statemrnt
+x=43
+y=76
+z=32
+if x==y:
+    print("i am a student.")
+    if z==x:
+             print("i am graduated student")
+    elif y>z: 
+             print("i am not graduated student")
+elif x>z:
+    print("i am a topper student")
+else:
+    print("i am average student.") 
+# program no 2:
+# nested if else statement:
+a=20
+b=76
+c=30
+if a<=b:
+     print("congratulations you are seleced")
+     if b==a:
+           print("interview is held")
+     elif b==c:
+            print ("meeting cancel")
+elif a>=b:
+            print("interview is not held")
+else:
+  print("congratulations you are pass with Good marks and selected in interview")
+
+#   program no 3:
+#   for loop statements:
+for x in range(11):
+    print (x)
+
+# program no 4:
+for i in range(4):
+    print (i)
+    
+
+
+       
